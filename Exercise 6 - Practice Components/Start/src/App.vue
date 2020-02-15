771c9b66ee946c7948b069377f7e0a99c487ecb7
DESKTOP-M5A0PQU\Neli <kondovaneli@gmail.com>
2020-02-16 01:44:28 +0200

    Assignment 6 - Components

---
+++ Exercise 6 - Practice Components/Start/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <my-header></my-header>
+    <hr />
+    <app-content></app-content>
+    <hr />
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+import Content from "./Content.vue";
+import Header from "./Header.vue";
+
+export default {
+  components: {
+    "my-footer": Footer,
+    "app-content": Content,
+    "my-header": Header
+  }
+};
+</script>
+
+<style>
+</style>
