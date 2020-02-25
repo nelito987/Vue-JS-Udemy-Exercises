8a3502277ec2bb3c0dd315f5d112250aae47ebfc
DESKTOP-M5A0PQU\Neli <kondovaneli@gmail.com>
2020-02-26 00:29:42 +0200

    Assignment 8 slots and  dynamic components

---
+++ Exercise 8 - Slots and Dynamic components/Start/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+        <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+        <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+        <hr />
+        <keep-alive>
+          <component :is="selectedComponent">
+              <p class="slotStyle">Here is the slot text</p>
+          </component>
+        </keep-alive>
+        <!-- <app-blue>
+          <p>some text in blue paragraph</p>
+        </app-blue>
+        <app-green>Green paragraph</app-green>
+        <app-red>
+          <p>Red paragraph</p>
+        </app-red> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
+
+export default {
+  data: function() {
+    return {
+      selectedComponent: "appBlue"
+    };
+  },
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  }
+};
+</script>
+
+<style>
+.slotStyle{
+    font-weight: bold;
+    font-style: italic;
+}
+</style>
---
+++ Exercise 8 - Slots and Dynamic components/Start/src/components/Red.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <slot></slot>
+    <p>Some text in the RED paragraph</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+div {
+  border: 1px solid red;
+  background-color: lightcoral;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+}
+</style>
