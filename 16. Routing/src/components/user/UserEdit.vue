62dfcb6c2bed5a720350c87701bb09c2bdf22a40
DESKTOP-M5A0PQU\Neli <kondovaneli@gmail.com>
2020-03-12 12:53:47 +0200

    Stock trader project - routes setup

--- 16. Routing/src/components/user/UserEdit.vue
+++ 16. Routing/src/components/user/UserEdit.vue
@@ -13,6 +13,11 @@
 <script>
 //example of beforeRouteLeave. If the 'Confirm' button is not click, 
 //a confirmaiton dialog will appear when the 'Go to Home page' is clicked
+
+//Note that beforeRouteEnter is the only guard that supports passing a callback to next. 
+//For beforeRouteUpdate and beforeRouteLeave, this is already available, 
+//so passing a callback is unnecessary and therefore not supported
+
 export default {
   data() {
     return {
---
+++ 18. Final Project - Stock Trader/stock-trader/src/components/stocks/Stock.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Stock stock Component</h1>
+</template>
